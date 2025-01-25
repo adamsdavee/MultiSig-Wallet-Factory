c97d697693fd527de4d69b01721c0e678cc9cef3
David Chukwudi Onyeka (Nokia) <david.chukwudi_onyeka@nokia.com>
2025-01-25 16:50:44 +0100

    multisig

--- Frontend/app/client.ts
+++ Frontend/app/client.ts
@@ -2,5 +2,5 @@
 import { createThirdwebClient } from "thirdweb";
 
 export const client = createThirdwebClient({
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
+  clientId: "abf285902676f10739931719171a6b4c",
 });
